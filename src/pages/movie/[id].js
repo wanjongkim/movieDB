12392d2f903a0678b6e080b1fed16634f5d8fef3
Wan Jong Kim <wkim2@9152.local>
2023-05-25 13:57:28 -0400

    Fixed the pathing of stylesheet for the movie [id] page

--- src/pages/movie/[id].js
+++ src/pages/movie/[id].js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import TopNav from "../components/topNav";
-import styles from "../../styles/Home.module.css"
+import styles from "../styles/Home.module.css"
 import TopBilledCast from "../../components/topBilledCast";
 import Link from "next/link";
 
